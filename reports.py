88a581892abfc29b7993ce8a9f92969a8bc8b879
jiakaixiao <jiakaixiao@tencent.com>
2020-05-21 17:18:55 +0800

    1.2018深圳市基金会

--- reports.py
+++ reports.py
@@ -22,74 +22,12 @@ headers={
     "Content-Type": "application/x-www-form-urlencoded"
 }
 
+SESSIONID = "0E6EF88C5BE69C37B536BFD783317E5A"
+
 def sleep(s=1,e=2):
     s = random.randint(s,e)
     print("sleep for %d seconds" % s)
     time.sleep(s)
-
-def shanghaiFunds():
-    # http://www.chinanpo.gov.cn/search/poporg.html?i=1143515&u=53310000MJ495410XG
-    headers={
-        "Host": "www.chinanpo.gov.cn",
-        "User-Agent":"Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.146 Safari/537.36",
-        "Origin": "http://www.chinanpo.gov.cn",
-        "Content-Type": "application/x-www-form-urlencoded"
-    }
-    proxy = {
-        "http": "127.0.0.1:12639",
-        "https": "127.0.0.1:12639",
-    }
-    url = "http://www.chinanpo.gov.cn/search/orgcx.html"
-
-    import re
-    # funds = set()
-    # for page in range(1,344):
-    #     params = dict(tabIndex=2,t=3,
-    #         orgName="%E4%B8%8A%E6%B5%B7",unifiedCode='',regName='',legalName='',
-    #         supOrgName='',corporateType='',status=2,orgAddNo=31,regNum=-2,regDate='',regDateEnd='',
-    #         page_flag="true", pagesize_key="usciList",
-    #         goto_page="first" if page==1 else "next",current_page=page,total_count=17109,page_size=50
-    #     )
-    #     r = requests.post(url, params, proxies=proxy, headers= headers)
-    #     if r.status_code != 200:
-    #         continue
-    #     # sleep()
-    #     records = list(re.findall('popOrgWin\(\"(\d+)\",\"(\S+)\"\)',r.text))
-    #     print("Parsing page {}, {} records found.".format(page, len(records)))
-    #     for p in records:
-    #         funds.add("+".join(p))
-        
-    # with open("data/shanghai.txt","w",encoding="utf8") as writer:
-    #     writer.writelines("\n".join([" ".join(p.split("+")) for p in funds]))
-    
-    url = "http://www.chinanpo.gov.cn/search/poporg.html"
-    with open("data/shanghai.txt","r",encoding="utf8") as reader:
-        records = [tuple(l.strip().split(" ")) for l in reader]
-
-        lines = []
-        for idx,(i,u) in enumerate(records):
-            if (idx+1) % 500 == 0:
-                with open("data/shanghai.csv","a",encoding="utf8") as writer:
-                    writer.writelines("\n".join(lines))
-                lines = []
-
-            print("Getting data from {}/{}".format(idx+1, len(records)))
-            r = requests.get(url, dict(i=i,u=u), proxies=proxy)
-            if r.status_code != 200:
-                continue
-            soup = BS(r.text, "html.parser")
-            
-            title = re.search("\S+",soup.find("div",{"class": "title_bg"}).text).group()
-            title = re.sub("\r|\n|,"," ", title)
-            td = re.sub("\r|\n|,",' ',soup.findAll("td")[-1].text.strip())
-            # print(title)
-            # print(td)
-            lines.append("{},{}".format(title,td))
-            # break
-            # sleep()
-
-        with open("data/shanghai.csv","a",encoding="utf8") as writer:
-            writer.writelines("\n".join(lines))
 
 def shenzhenReportLinks():
     ofile = "declare_ids.csv"
@@ -110,54 +48,23 @@ def shenzhenReportLinks():
     df = pd.concat(dfs)
     df.to_csv(ofile,index=False)
 
-    # try:
-    #     bar = Bar(940-42+1).start()
-    #     for page in range(42, 940 + 1):
-    #         bar.update(bar.currval+1)
-    #         if page in pages:
-    #             continue
-            
-    #         #"pageNum":42,"pageSize":10,"size":10,"startRow":411,"endRow":420,"total":28843,"pages":2885
-            
-    #         params = dict(pageNum=4, pageSize=100, size=100)
-    #         r = requests.post(url, params, proxies=proxy, headers= headers)
-    #         if r.status_code != 200:
-    #             continue
-
-    #         data = json.loads(r.text)
-    #         if data["msg"] != "成功":
-    #             raise ValueError(data["msg"])
-
-    #         print(data)
-    #         print("pageNum=<{}> pageSize=<{}> size=<{}> startRow=<{}> pages=<{}>".format(data["pageNum"],data["pageSize"],data["size"],data["startRow"],data["pages"]))
-    #         records = data["data"]["list"]
-
-    #         # SOCIETY_NAME: 深圳市审计学会
-    #         # CREDIT_CODE: 5144030050267905XD
-    #         # REPORTYEAR: 2019
-    #         # ID: 7157b521759d40818f474f3933786af1
-    #         # DECLAREID: f930004b253c47ab86c97d0074ebc303
-    #         break
-    #     bar.finish()
-    # except Exception as ex:
-    #     raise ex
-    # finally:
-    #     pass
-
-    #     params = dict(tabIndex=2,t=3,
-    #         orgName="%E4%B8%8A%E6%B5%B7",unifiedCode='',regName='',legalName='',
-    #         supOrgName='',corporateType='',status=2,orgAddNo=31,regNum=-2,regDate='',regDateEnd='',
-    #         page_flag="true", pagesize_key="usciList",
-    #         goto_page="first" if page==1 else "next",current_page=page,total_count=17109,page_size=50
-    #     )
-    #     r = requests.post(url, params, proxies=proxy, headers= headers)
-    #     if r.status_code != 200:
-    #         continue
-
 def shenzhenReports():
-    # ofile = "declare_ids_2018.csv"
-    # agents = pd.read_csv(ofile)
+    ofile = "declare_ids_2018.csv"
+    agents = pd.read_csv(ofile)
+    agents = agents[agents.SOCIETY_NAME.map(lambda r: isinstance(r,str) and "基金会" in r)]
 
+    path = "funds"
+
+    names = set()
+    for file in os.listdir(path):
+        if os.path.isdir(os.path.join(path,file)):
+            continue
+        groups = re.search("(.*)-(.*).html", file)
+        if groups is None:
+            continue
+        name = groups.group(1)
+        names.add(name)
+    
     # <iframe  name="首页" src="/SOCSP_O/njnbMf/toIndex?declareId=04fde8135bd04309a6b5c864fd5e1dc9&formId=483cb448150749c4876f79a471960aee&print=1&dy=1"  marginheight="0" marginwidth="0" frameborder="0" scrolling="none" style="visibility:hidden;width:100%; overflow:auto; border-bottom:0px solid blue" ></iframe>
     # <iframe  name="基本信息" src="/SOCSP_O/njnbMf/toJbxx?declareId=04fde8135bd04309a6b5c864fd5e1dc9&formId=e75f654cbe124a7992e55add4066ae35&print=1&dy=1"  marginheight="0" marginwidth="0" frameborder="0" scrolling="none" style="visibility:hidden;width:100%; overflow:auto; border-bottom:0px solid blue" ></iframe>
     # <iframe  name="监事会信息" src="/SOCSP_O/njnbMf/toZzjsJs?declareId=04fde8135bd04309a6b5c864fd5e1dc9&formId=29ab3ebe15734ae1a2f198a3f33ceda6&print=1&dy=1"  marginheight="0" marginwidth="0" frameborder="0" scrolling="none" style="visibility:hidden;width:100%; overflow:auto; border-bottom:0px solid blue" ></iframe>
@@ -180,37 +87,171 @@ def shenzhenReports():
     # <iframe  name="业务开展情况" src="/SOCSP_O/njnbJjh/toYwkzqk?declareId=04fde8135bd04309a6b5c864fd5e1dc9&formId=089d349df95645d783779ab079face11&print=1&dy=1"  marginheight="0" marginwidth="0" frameborder="0" scrolling="none" style="visibility:hidden;width:100%; overflow:auto; border-bottom:0px solid blue" ></iframe>
     # <iframe  name="公益项目收支" src="/SOCSP_O/njnbMf/toGyxmsz?declareId=04fde8135bd04309a6b5c864fd5e1dc9&formId=da10eca2d46e4ef6a708d5430a09094c&print=1&dy=1"  marginheight="0" marginwidth="0" frameborder="0" scrolling="none" style="visibility:hidden;width:100%; overflow:auto; border-bottom:0px solid blue" ></iframe>
     # <iframe  name="公益活动支出" src="/SOCSP_O/publicnjnb/toGyhdzc?declareId=04fde8135bd04309a6b5c864fd5e1dc9&formId=2206dc51892b455d87fde7dbffd68154&print=1&dy=1"  marginheight="0" marginwidth="0" frameborder="0" scrolling="none" style="visibility:hidden;width:100%; overflow:auto; border-bottom:0px solid blue" ></iframe>
-#     from selenium import webdriver#导入库
-    
-#     chrome_options = webdriver.ChromeOptions()
-#     chrome_options.add_argument('--proxy-server=127.0.0.1:12639')
-# #
-#     browser = webdriver.Chrome("E:/chromedriver_win32/chromedriver.exe",options=chrome_options)
 
-    url = "http://218.17.83.146:9009/SOCSP_O/publicnjnb/printAll?declareId="
+    #url = "http://218.17.83.146:9009/SOCSP_O/publicnjnb/printAll?declareId="
+    url = "http://218.17.83.146:9009"
+
     settings = [
-        ("资产负债表","/SOCSP_O/publicnjnb/topublicZcfz?formId=31968c50628b456a9869de3a84f4d05d&print=1&dy=1&declareId=")
+        ("资产负债表","/SOCSP_O/publicnjnb/topublicZcfz?formId=31968c50628b456a9869de3a84f4d05d&print=1&dy=1&declareId="),
+        ("业务活动表","/SOCSP_O/publicnjnb/topublicYwhd?formId=089d349df95645d783779ab079face11&print=1&dy=1&declareId=")
     ]
-    name,did = ('深圳市宝安区新安街道幸福海岸幼儿园','04fde8135bd04309a6b5c864fd5e1dc9')
-    #for name,did in ():
+    failed = 0
 
-    #print(url + settings[0][1] + did)
-    # url_ = url + settings[0][1] + did
-    # print(url_)
-    # browser.get(url_)#打开浏览器预设网址
-    # print(browser.page_source)#打印网页源代码
-    
-    # break 
 
-    url_ = url +  did
-    print(url_)
-    r = requests.get(url_, proxies = proxy, headers = headers, cookies={"JSESSIONID": "5F96AFA08B4D31EBE31969A8D6AF2C69"})
-    if r.status_code != 200:
-        #continue
-        pass
-    print(r.text)
-    # browser.close()#关闭浏览器
+    # from selenium import webdriver
+    # chromeOptions = webdriver.ChromeOptions()
+    # chromeOptions.add_argument('--proxy-server=http://127.0.0.1:12639')
+    # #chromeOptions.add_argument("user-data-dir=C:\\Users\\jiakaixiao\\AppData\\Local\\Google\\Chrome\\User Data\\Default")
+    # #chromeOptions.add_argument("--user-data-dir='C:/Users/jiakaixiao/AppData/Local/Google/Chrome/User Data/Default'");
+    # driver= webdriver.Chrome("E:/chromedriver_win32/chromedriver.exe",options=chromeOptions)
+
+    for i,(name,did) in enumerate(zip(agents.SOCIETY_NAME,agents.DECLAREID)):
+        
+        if isinstance(name,str)==False or isinstance(did,str)==False or name is None or did is None or not name.strip() or not did.strip() or name.strip() in names:
+            continue
+        if "基金会" not in name:
+            continue
+
+        name, did = name.strip(), did.strip()
+
+        print("Loading <{}/{}>: {} {}".format(i+1,agents.shape[0], name, did))
+        
+        contents = []
+        try:
+            #url_ = url + postfix +  did
+            url_ = url + "/SOCSP_O/publicnjnb/printAll?declareId=" + did
+
+            r = requests.get(url_, proxies = proxy, headers = headers, cookies={"JSESSIONID": SESSIONID})
+            if r.status_code != 200:
+                continue
+
+            zcfz = None
+            p = "/SOCSP_O/publicnjnb/topublicZcfz.*dy=1"
+            g = re.search(p, r.text)
+            if g:
+                zcfz = url+g.group(0)
+
+            ywhd = None
+            p = "/SOCSP_O/publicnjnb/topublicYwhd.*dy=1"
+            g = re.search(p, r.text)
+            if g:
+                ywhd = url+g.group(0)
+
+            if not zcfz or not ywhd:
+                continue
+
+            print("=" * 120)
+            print("Crawling for <{}>".format(name))
+            for tname, link in [("资产负债表",zcfz),("业务活动表",ywhd)]:
+                print("<{}>: {}".format(tname, link))
+                r = requests.get(link, proxies = proxy, headers = headers, cookies={"JSESSIONID": SESSIONID})
+                if r.status_code != 200:
+                    break
+                
+                contents.append((tname, r.text))
+            
+            # driver.implicitly_wait(10)
+            # driver.get(url_)
+            # driver.add_cookie({"name": "JSESSIONID", "value": SESSIONID, "domain": "218.17.83.146", "path": "/SOCSP_O"})
+            # time.sleep(5)
+            # contents.append((tname, driver.page_source))
+        except Exception as ex:
+            failed += 1
+        finally:
+            if len(contents) == 2:
+                for tname, content in contents:
+                    with open("{}/{}-{}.html".format(path,name,tname),"w", encoding="utf8") as writer:
+                        writer.write(content)
+        # if i>=2:
+        #     break
+        sleep()
+
+    #driver.quit()
+    print("Failed files in total <{}>".format(failed))
+
+def parse_reports():
+    path = "reports"
+    path = "funds"
+
+    failed = 0
+    records = {}
+    for i, file in enumerate(os.listdir(path)):
+        if os.path.isdir(os.path.join(path,file)):
+            continue
+        groups = re.search("(.*)-(.*).html", file)
+        if groups is None:
+            continue
+        name = groups.group(1)
+        tname = groups.group(2)
+        
+        try:
+            if tname == "资产负债表":
+                with open(os.path.join(path, file),"r",encoding="utf8") as reader:
+                    data = reader.read()
+                    
+                    soup = BS(data, "html.parser")
+                    rows = soup.find("table", {"id": "tablepr"}).findAll("tr") 
+                    
+                    values = {
+                    "组织名称": name,
+                    "资产总计": "",
+                    "净资产合计": "",
+                    "非限制性净资产": "",
+                    "货币资金": "",
+                    "短期投资": "",
+                    "长期股权投资": "",
+                    "长期债权投资": ""
+                    }
+                    for r in rows:
+                        fields = tuple(td.text.strip() for td in r.findAll("td"))
+                        #print(fields)
+                        for n,rid,_,v in [fields[:4], fields[4:]]:
+                            if n in values:
+                                values[n] = v
+                    
+                    if name in records:
+                        records[name].update(values)
+                    else:
+                        records[name] = values
+
+            if tname == "业务活动表":
+                with open(os.path.join(path, file),"r",encoding="utf8") as reader:
+                    data = reader.read()
+                    
+                    soup = BS(data, "html.parser")
+                    rows = soup.find("table", {"id": "tablepr"}).findAll("tr") 
+
+                    values = {
+                    "组织名称": name,
+                    "捐赠收入": "",
+                    "政府补助收入": "",
+                    "提供服务收入": "",
+                    "投资收益": "",
+                    }
+
+                    for r in rows:
+                        fields = tuple(td.text.strip() for td in r.findAll("td"))
+                        if len(fields) != 8:
+                            continue
+                        if fields[0] == "其中：捐赠收入":
+                            values["捐赠收入"] = fields[-1]
+                        elif fields[0] in values:
+                            values[fields[0]] = fields[-1]
+
+                    if name in records:
+                        records[name].update(values)
+                    else:
+                        records[name] = values
+        except Exception as ex:
+            print("Exception parsing <{}>".format(file))
+            failed += 1
+    print("Failed files in total <{}>".format(failed))
+
+    df = pd.DataFrame(records.values()) #[["组织名称","资产总计","净资产合计","非限制性净资产","货币资金","短期投资","长期股权投资","长期债权投资","捐赠收入","政府补助收入","提供服务收入","投资收益"]]
+    df.to_csv("result.csv",index=False)
+
 
 if __name__ == '__main__':
     # shenzhenReportLinks()
-    shenzhenReports()
+    # shenzhenReports()
+    parse_reports()
